88cc38089d4168cf511256a4717458a4a4700edb
John Brennan <jbrennan@fluentstream.com>
2019-10-14 20:02:33 -0600

    Updates

--- precache-manifest.2a4e97ca19e92fdaf86e37996cd092cb.js
+++ precache-manifest.2a4e97ca19e92fdaf86e37996cd092cb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "790232669eecf28951e5a80a39c6f1cc",
+    "revision": "be49649e05df81e69a622bb5ff71a7d0",
     "url": "/jpb55116/weather-wardrobe/index.html"
   },
   {
-    "revision": "1193d0441751cd6b80ff",
-    "url": "/jpb55116/weather-wardrobe/static/css/main.34de6062.chunk.css"
+    "revision": "78cb2cb35b101f169e6b",
+    "url": "/jpb55116/weather-wardrobe/static/css/main.bb3ed7ad.chunk.css"
   },
   {
     "revision": "e716463962ab26d4a5c9",
     "url": "/jpb55116/weather-wardrobe/static/js/2.2a098cf8.chunk.js"
   },
   {
-    "revision": "1193d0441751cd6b80ff",
-    "url": "/jpb55116/weather-wardrobe/static/js/main.7b08c1a5.chunk.js"
+    "revision": "78cb2cb35b101f169e6b",
+    "url": "/jpb55116/weather-wardrobe/static/js/main.88df4072.chunk.js"
   },
   {
     "revision": "430b2a904ed48f42bbc0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jpb55116/weather-wardrobe/precache-manifest.a54c0039dcef9d9c66ce3fb6ce91000d.js"
+  "/jpb55116/weather-wardrobe/precache-manifest.2a4e97ca19e92fdaf86e37996cd092cb.js"
 );
 
 self.addEventListener('message', (event) => {
